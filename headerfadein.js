332540d915ffd04f2bbe0622dffb325de2e9c19a
JunKai <jkgoh5488@gmail.com>
2023-11-29 17:25:47 +0800

    fedassignment

---
+++ headerfadein.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var headerImage = document.getElementById("header-image");
+    headerImage.style.opacity = 1; // Make the image visible
+});
